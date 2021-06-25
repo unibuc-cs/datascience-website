5514515fa62d09aaef66bc4c76cbee3c5c611c88
Alin Stefanescu <alinstef@users.noreply.github.com>
2021-06-25 15:24:57 +0300

    added text to the projects

--- src/projects.js
+++ src/projects.js
@@ -4,12 +4,12 @@ let projects = [{
     'sponsor': 'BRD – Groupe Societe Generale',
     'budget': '39,000 euros',
     'partners': 'UB, BRD',
-    'timeline': 'June - December 2021',
-    'short_description': 'Investigate explainable AI and anomaly detection for banking system',
+    'timeline': 'July - December 2021',
+    'short_description': 'Investigate explainable AI, financial trading optimization anomaly detection for banking system',
     'paragraphs': [
-        'The project consisted of short (2019 - 2020) and medium (2019 - 2023) time forecasts for the evolution of the Romanian pharmaceutical market. Concretely, it consisted of monthly forecasting of three key market indicators (total number of days of treatment, total number of units sold and total sales value) both at the level of the entire market and across different market segments of interest.',
-        'The chosen solution - given the available data and certain time constraints - consisted of individual time series modeling, one for each key market indicator and each market segment of interest. As such, over a couple of dozen models were employed, based on a widely accepted and up to date time series modeling framework.',
-        'Before the outbreak of the COVID 19 pandemic, the project was planned to have a second phase based on additional 3rd party data and state of the art deep learning techniques.',
+       'Explainable AI for court decision models: We will explore the decision process of artificial intelligence models developed to predict court decisions for lawsuits and for the financial domain. The goal is to understand how the model works and to provide explanations for individual model decisions. Ideally, the research should find general features that the model pays the most attention to, synthesizing exemplar inputs and showcasing deciding factors for a particular prediction.',
+       'Financial trading optimization: The recent financial trading techniques rely on artificial intelligence models that explore how to maximize the profit through setting optimal prices. One purpose of this research is the identification of the best learning model that could maintain a balance between thresholds that are attractive for the user and those that generate maximal profit. Secondly, detecting what features of the user entities that are most important for the model would provide explanations for the optimal decisions. By showcasing deciding factors for a particular prediction will be also important.',
+       'Anomaly detection in computer networks traffic: We plan to explore the continuous monitoring and analysis of network traffic (data packet information, meta-data, and signatures) for unusual isolated events or long-term trends that are developing. The goal is the early identification of potential security threats and their immediate mitigation using state-of-the-art anomaly detection algorithms developed in the machine learning community. Ideally, real-time identification of anomalies in the computer network traffic will lead to early detection of security threats.',
     ]
 }, {
     'id': 'brd2019',
@@ -20,15 +20,14 @@ let projects = [{
     'timeline': 'July - October 2019',
     'short_description': 'This is some text made out of 3 sentences. This is the second one and has some meaning',
     'paragraphs': [
-        'The project consisted of short (2019 - 2020) and medium (2019 - 2023) time forecasts for the evolution of the Romanian pharmaceutical market. Concretely, it consisted of monthly forecasting of three key market indicators (total number of days of treatment, total number of units sold and total sales value) both at the level of the entire market and across different market segments of interest.',
-        'The chosen solution - given the available data and certain time constraints - consisted of individual time series modeling, one for each key market indicator and each market segment of interest. As such, over a couple of dozen models were employed, based on a widely accepted and up to date time series modeling framework.',
-        'Before the outbreak of the COVID 19 pandemic, the project was planned to have a second phase based on additional 3rd party data and state of the art deep learning techniques.',
+        'Predicting court decisions for lawsuits: Using data obtained from previous civil lawsuits and court decisions, machine learning algorithms and natural language processing techniques were investigated in order to decide what are the chances of winning an ongoing lawsuit. Legal suggestions that may increase the chances of winning were also offered in a preliminary form.',
+        'Anomaly detection in financial transactions: Using data from the financial flow between various bank accounts, algorithms for detection of anomalies and implicitly of suspicious transactions were investigated. Reporting such anomalies helps to increase the efficiency of internal processes and to reduce financial operational risk.',
     ]
 }, {
     'id': 'humanoidid',
     'title': 'Humanoid ID',
-    'sponsor': 'Romanian Ministry of Research, Innovation, and Digitalization (grant no 32PTE/2020',
-    'budget': '150,000 euros',
+    'sponsor': 'Romanian Ministry of Research, Innovation, and Digitalization (grant no 32PTE/2020)',
+    'budget': '275,000 euros',
     'partners': 'University of Bucharest, Tremend',
     'timeline': 'October 2020 - May 2022',
     'short_description': 'AI-driven identity verification and document authenticity validation',
@@ -42,39 +41,39 @@ let projects = [{
     'title': 'MASSA',
     'sponsor': 'Romanian Ministry of Research, Innovation, and Digitalization',
     'budget': '47,000 euros',
-    'partners': 'Univeristy of Bucharest, Municipality of Bucharest, Bolt',
+    'partners': 'University of Bucharest, Municipality of Bucharest, Bolt',
     'timeline': 'November 2020 - May 2022',
-    'short_description': 'Traffic optimization using state-of-the-art machine learining algorithms',
+    'short_description': 'Traffic optimization using state-of-the-art machine learining algorithms (classic machine learning, reinforcement learning, dictionary learning',
     'paragraphs': [
-        'The project consisted of short (2019 - 2020) and medium (2019 - 2023) time forecasts for the evolution of the Romanian pharmaceutical market. Concretely, it consisted of monthly forecasting of three key market indicators (total number of days of treatment, total number of units sold and total sales value) both at the level of the entire market and across different market segments of interest.',
-        'The chosen solution - given the available data and certain time constraints - consisted of individual time series modeling, one for each key market indicator and each market segment of interest. As such, over a couple of dozen models were employed, based on a widely accepted and up to date time series modeling framework.',
-        'Before the outbreak of the COVID 19 pandemic, the project was planned to have a second phase based on additional 3rd party data and state of the art deep learning techniques.',
+        'Traffic optimization using state-of-the-art machine learining algorithms, from classic machine learning to reinforcement learning and dictionary learning',
+        'We use data science methods, involving with data collection, preparations and transformation of data, application of the above learning algorithms, optimization models and finally testing and simulation on use case as close to reality as possible',
+        'The data we plan to use comes from the Municipality of Bucharest (a variaty of data from intersections, road sensors etc), but also from drivers of the smart mobility company Bolt.',
     ]
 }, {
     'id': 'norwegian1',
-    'title': 'Norwegian 1',
-    'sponsor': 'Romanian Ministry of Research, Innovation, and Digitalization',
-    'budget': '47,000 euros',
-    'partners': 'Univeristy of Bucharest, Municipality of Bucharest, Bolt',
+    'title': 'Norwegian 1 TODO',
+    'sponsor': 'Norwegian Ministry of Research, TODO',
+    'budget': '347,000 euros',
+    'partners': 'University of Bucharest, Polytechnic University of Bucharest, TODO',
     'timeline': 'November 2020 - May 2022',
-    'short_description': 'Traffic optimization using state-of-the-art machine learining algorithms',
+    'short_description': 'TODO',
     'paragraphs': [
-        'The project consisted of short (2019 - 2020) and medium (2019 - 2023) time forecasts for the evolution of the Romanian pharmaceutical market. Concretely, it consisted of monthly forecasting of three key market indicators (total number of days of treatment, total number of units sold and total sales value) both at the level of the entire market and across different market segments of interest.',
-        'The chosen solution - given the available data and certain time constraints - consisted of individual time series modeling, one for each key market indicator and each market segment of interest. As such, over a couple of dozen models were employed, based on a widely accepted and up to date time series modeling framework.',
-        'Before the outbreak of the COVID 19 pandemic, the project was planned to have a second phase based on additional 3rd party data and state of the art deep learning techniques.',
+        'TODO', 
+        'TODO',
+        'TODO',
     ]
 }, {
     'id': 'norwegian2',
-    'title': 'Norwegian 1',
-    'sponsor': 'Romanian Ministry of Research, Innovation, and Digitalization',
-    'budget': '47,000 euros',
-    'partners': 'UB, Municipality of Bucharest, Bolt',
+    'title': 'Norwegian 2 TODO',
+    'sponsor': 'Norwegian Ministry of Research, TODO',
+    'budget': '347,000 euros',
+    'partners': 'University of Bucharest, Polytechnic University of Bucharest, TODO',
     'timeline': 'November 2020 - May 2022',
-    'short_description': 'Traffic optimization using state-of-the-art machine learining algorithms',
+    'short_description': 'TODO',
     'paragraphs': [
-        'The project consisted of short (2019 - 2020) and medium (2019 - 2023) time forecasts for the evolution of the Romanian pharmaceutical market. Concretely, it consisted of monthly forecasting of three key market indicators (total number of days of treatment, total number of units sold and total sales value) both at the level of the entire market and across different market segments of interest.',
-        'The chosen solution - given the available data and certain time constraints - consisted of individual time series modeling, one for each key market indicator and each market segment of interest. As such, over a couple of dozen models were employed, based on a widely accepted and up to date time series modeling framework.',
-        'Before the outbreak of the COVID 19 pandemic, the project was planned to have a second phase based on additional 3rd party data and state of the art deep learning techniques.',
+        'TODO', 
+        'TODO',
+        'TODO',
     ]
 }, {
     'id': 'pharma',
@@ -83,7 +82,7 @@ let projects = [{
     'budget': '9,000 euros',
     'partners': 'Univeristy of Bucharest, RODA',
     'timeline': 'July - October 2019',
-    'short_description': 'This is some text made out of 3 sentences. This is the second one and has some meaning. And this is the third one and should end the description.',
+    'short_description': 'Phamaceutical market forcasting along several financial dimensions',
     'paragraphs': [
         'The project consisted of short (2019 - 2020) and medium (2019 - 2023) time forecasts for the evolution of the Romanian pharmaceutical market. Concretely, it consisted of monthly forecasting of three key market indicators (total number of days of treatment, total number of units sold and total sales value) both at the level of the entire market and across different market segments of interest.',
         'The chosen solution - given the available data and certain time constraints - consisted of individual time series modeling, one for each key market indicator and each market segment of interest. As such, over a couple of dozen models were employed, based on a widely accepted and up to date time series modeling framework.',
@@ -92,15 +91,15 @@ let projects = [{
 }, {
     'id': 'river',
     'title': 'Smart Fuzz Testing',
-    'sponsor': 'Romanian Ministry of Research, Innovation, and Digitalization',
-    'budget': '120,000 euros',
-    'partners': 'University of Bucharest, Polytechnic University of Bucharest, Bitdefender',
+    'sponsor': 'Romanian Ministry of Research, Innovation, and Digitalization (several grants)',
+    'budget': '720,000 euros',
+    'partners': 'University of Bucharest, Bitdefender, Polytechnic University of Bucharest',
     'timeline': 'October 2016 - September 2022',
     'short_description': 'An open-source framework that uses AI to guide the fuzz testing of binary programs and IoT systems',
     'paragraphs': [
-        'The project consisted of short (2019 - 2020) and medium (2019 - 2023) time forecasts for the evolution of the Romanian pharmaceutical market. Concretely, it consisted of monthly forecasting of three key market indicators (total number of days of treatment, total number of units sold and total sales value) both at the level of the entire market and across different market segments of interest.',
-        'The chosen solution - given the available data and certain time constraints - consisted of individual time series modeling, one for each key market indicator and each market segment of interest. As such, over a couple of dozen models were employed, based on a widely accepted and up to date time series modeling framework.',
-        'Before the outbreak of the COVID 19 pandemic, the project was planned to have a second phase based on additional 3rd party data and state of the art deep learning techniques.',
+        'We developed an open-source framework, called River, that uses AI to guide the fuzz testing of binary programs and of IoT systems.',
+        'RIVER can test vulnerabilities of binary files using AI, especially reinforcement learning, combined with taint analysis, symbolic and concolic execution.',
+        'Our platform is currently extended to be able to evaluate smart home interconnected applications based on beyond-state-of-the-art methods for software security assessment.',
     ]
 }, ];
 
